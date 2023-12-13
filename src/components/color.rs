9c55bd9d3df1f884d90d0281a09e55712b828e94
Arjun Nayak <49101235+Squidtoon99@users.noreply.github.com>
2023-12-13 02:58:42 -0600

    Update color.rs

--- src/components/color.rs
+++ src/components/color.rs
@@ -1,17 +1,19 @@
 use crate::component::{Component as ComponentTrait, ComponentInput};
 use crate::error::InteractionError;
 use crate::input::SharedInput;
-
+use crate::utils::bookmark_data;
+use twilight_model::channel::Message;
 use twilight_model::channel::message::{
     component::{ActionRow, Button, ButtonStyle},
     Component,
 };
 
 use async_trait::async_trait;
-use twilight_model::channel::message::{ReactionType};
+use twilight_model::channel::message::{ReactionType, MessageFlags, Embed};
 use twilight_model::http::interaction::{InteractionResponse, InteractionResponseType};
 use twilight_model::id::Id;
 use twilight_util::builder::InteractionResponseDataBuilder;
+use twilight_util::builder::embed::EmbedBuilder;
 use worker::console_log;
 
 pub(crate) struct Color {}
@@ -28,30 +30,70 @@ impl ComponentTrait for Color {
 
         if custom_id.contains(":") {
             let (_name, data) = custom_id.split_once(":").unwrap();
-            let (color, url) = data.split_once(":").unwrap();
-            let url = format!("https://discord.com/channels/{}", url);
-            console_log!("color: {}, url: {}", color, url);
-            let msg = input.message.unwrap();
-            let mut embeds = msg.embeds.clone();
+            let (action, url) = data.split_once(":").unwrap();
+            let actual_url = format!("https://discord.com/channels/{}", &url);
+            console_log!("action: {}, url: {}", action, url);
+            let mut embeds: Vec<Embed>;
+            let mut kind = InteractionResponseType::UpdateMessage;
+            match action {
+                "update" => {
+                    // fetch new bookmark data
+                    let client = input.http_client()?;
+                    let mut parts = url.splitn(3, "/");
+                    let (guild_id, channel_id, message_id) = (parts.next().unwrap(), parts.next().unwrap(), parts.next().unwrap());
+                    let url = format!("https://discord.com/api/v10/channels/{}/messages/{}", channel_id, message_id);
+                    if let Ok(response) = client.get(url).send().await {
+                        let text  =response.text().await?;
+                        let msg_data: Message = serde_json::from_str(&text)?;
+                        embeds = bookmark_data(&msg_data, &client, guild_id).await?
+                    } else {
+                        return Ok(InteractionResponse {
+                            data: Some(
+                                InteractionResponseDataBuilder::new()
+                                    .embeds(vec![
+                                        EmbedBuilder::new()
+                                            .title("Message Edit Error")
+                                            .description("```\nI can no longer has access to the original message. It may have been deleted or hidden from me\n```")
+                                            .build()
+                                    ])
+                                    .flags(MessageFlags::EPHEMERAL)
+                                    .build()
+                                ),
+                            kind: InteractionResponseType::ChannelMessageWithSource
+                        })
+                    }
+                },
+                "repost" => {   
+                    let client = input.http_client()?;
+                    let (guild_id, _extra) = url.split_once("/").unwrap();
+                    
+                    embeds = bookmark_data(&input.message.unwrap(), &client, guild_id).await?;
+                    kind = InteractionResponseType::ChannelMessageWithSource;
+                },
+                color  => {
+                    // change the bookmark embeds color
+                    let msg = input.message.unwrap();
+                    embeds = msg.embeds.clone();
 
-            for mut embed in embeds.iter_mut() {
-                embed.color = Some(color.parse::<u32>().unwrap());
+                    for mut embed in embeds.iter_mut() {
+                        embed.color = Some(color.parse::<u32>().unwrap());
+                    }
+                    
+                }
             }
 
-            let default_components = input.default_components(url);
-
             Ok(InteractionResponse {
-                kind: InteractionResponseType::UpdateMessage,
+                kind: kind,
                 data: Some(
                     InteractionResponseDataBuilder::new()
                         .embeds(embeds)
-                        .components(default_components)
+                        .components(input.default_components(actual_url))
                         .content("")
                         .build(),
                 ),
             })
         } else {
-            let Some(Component::ActionRow(row)) = input.message.unwrap().components.last() else {
+            let Some(Component::ActionRow(row)) = input.message.unwrap().components.first() else {
                     return Err(InteractionError::WorkerError("No components found".to_string()))
                 };
             let Some(Component::Button(Button {url, ..})) = row.components.last() else {
@@ -62,14 +104,14 @@ impl ComponentTrait for Color {
                 let (_, data) = u.split_at(29);
                 data.to_string()
             };
-            let components = Component::ActionRow(ActionRow {
+            let color_components = Component::ActionRow(ActionRow {
                 components: vec![
                     Component::Button(Button {
                         style: ButtonStyle::Secondary,
                         label: None,
                         custom_id: Some(format!("color:5793266:{url}", url = url)),
                         emoji: Some(ReactionType::Custom {
-                            id: Id::new(1086977694312439869),
+                            id: Id::new(1184416010254684201),
                             name: Some("bk_blurple".to_string()),
                             animated: false
                         }),
@@ -82,7 +124,7 @@ impl ComponentTrait for Color {
                         custom_id: Some(format!("color:15548997:{url}", url = url)),
                         // <:bk_red:1086977702386466867>
                         emoji: Some(ReactionType::Custom {
-                            id: Id::new(1086977702386466867),
+                            id: Id::new(1184416103464714300),
                             name: Some("bk_red".to_string()),
                             animated: false
                         }),
@@ -95,7 +137,7 @@ impl ComponentTrait for Color {
                         custom_id: Some(format!("color:5763719:{url}", url = url)),
                         // <:bk_green:1086977699488223384>
                         emoji: Some(ReactionType::Custom {
-                            id: Id::new(1086977699488223384),
+                            id: Id::new(1184416108351070250),
                             name: Some("bk_green".to_string()),
                             animated: false
                         }),
@@ -108,7 +150,7 @@ impl ComponentTrait for Color {
                         label: None,
                         custom_id: Some(format!("color:16705372:{url}", url = url)),
                         emoji: Some(ReactionType::Custom {
-                            id: Id::new(1086977704194216066),
+                            id: Id::new(1184416111316435045),
                             name: Some("bk_yellow".to_string()),
                             animated: false
                         }),
@@ -121,7 +163,7 @@ impl ComponentTrait for Color {
                         label: None,
                         custom_id: Some(format!("color:15418782:{url}", url = url)),
                         emoji: Some(ReactionType::Custom {
-                            id: Id::new(1086977696216645657),
+                            id: Id::new(1184416105251471480),
                             name: Some("bk_fucahsia".to_string()),
                             animated: false
                         }),
@@ -130,9 +172,33 @@ impl ComponentTrait for Color {
                     }),
                 ],
             });
-
+            
+            let action_components = Component::ActionRow(ActionRow {
+                components: vec![
+                    Component::Button(Button {
+                        style: ButtonStyle::Primary,
+                        label: Some("Refresh Data".to_string()),
+                        custom_id: Some(format!("color:update:{url}", url = url)),
+                        emoji: Some(ReactionType::Unicode {
+                            name: "🔃".to_string()
+                        }),
+                        url: None,
+                        disabled: false
+                    }),
+                    Component::Button(Button {
+                        style: ButtonStyle::Primary,
+                        label: Some("Repost Bookmark".to_string()),
+                        custom_id: Some(format!("color:repost:{url}", url = url)),
+                        emoji: Some(ReactionType::Unicode {
+                            name: "📑".to_string()
+                        }),
+                        url: None,
+                        disabled: false
+                    }),
+                ]
+            });
             let data = InteractionResponseDataBuilder::new()
-                .components([components])
+                .components([color_components, action_components])
                 .build();
 
             Ok(InteractionResponse {
